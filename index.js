dcd9ae522368d439c6a67b4f2372577f7aa4a2a1
Ritsu.me <mail@ritsu-me.com>
2022-12-22 14:01:59 +0900

    added some packages

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ mongoose.connect(`mongodb+srv://admin:${process.env.MONGO_PW}@gms.o08ggq2.mongod
     console.error(error);
 });*/
 
-const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
+const { Client, GatewayIntentBits, ActivityType, EmbedBuilder } = require('discord.js');
 const client = new Client({
     intents: Object.values(GatewayIntentBits).reduce((a, b) => a | b)
 });
@@ -17,11 +17,41 @@ require("dotenv").config();
 client.once("ready", () => {
     client.user.setActivity({name: "Zone", type: ActivityType.Competing })
     console.log(`Logged in as ${client.user.tag}!!`)
+}).on("guildMemberAdd", (member) => {
+    client.channels.cache.get('1052798548997050422').send({
+        embeds: [
+            new EmbedBuilder()
+            .setAuthor({
+                name: member.displayName,
+                iconURL: member.displayAvatarURL({dynamic: true})
+            })
+        ]
+    })
 })
 
 client.on("messageCreate", (message) => {
     if(message.author.id == client.user.id)return;
     if(message.author.bot)return;
+    if(message.content == "z.emit"){
+        if(message.author.id !== "716343156513439845")return;
+        client.channels.cache.get('1052798548997050422').send({
+            embeds: [
+                new EmbedBuilder()
+                .setTitle('ユーザー情報')
+                .setDescription('ユーザー情報は以下の通りです。')
+                .setAuthor({ name: message.author.tag, iconURL: message.author.avatarURL({dynamic: true}) })
+                .setURL(`https://discord.com/users/${message.author.id}`)
+                .setThumbnail('https://gyazo.com/520c092f191cf3c7dcd75a559b7dd536/max_size/1000')
+                .setImage('https://p.kindpng.com/picc/s/108-1084174_discord-js-discord-js-logo-png-transparent-png.png')
+                .addFields({name:'json形式で書けるフィールド、こっちは名前で最大256字',value:'フィールドの値　※1024字まで'},{name:'`{name:"name",value:"value"}`で1セット',value:'足りないとエラーが出る'})
+                .addFields({name:'inline: trueを加えることで',value:'インラインにできる',inline:true})
+                .setColor("#2f3136")
+                .setFooter({text: '埋め込みのフッター 2048字まで\n埋め込み全体の文字数は6000字まで\n一つのメッセージで送れる埋め込みは10個'})
+                .setTimestamp()
+            ]
+        })
+        message.reply(toString(message.author.hexAccentColor))
+    }
 })
 
 client.login(process.env.BOT_TOKEN)
